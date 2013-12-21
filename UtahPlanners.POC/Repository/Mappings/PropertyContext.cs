e3790596f838746f2b7594fa542331509a38ccfd
landonpoch <landon.poch+git@gmail.com>
2013-12-21 17:41:18 -0600

    Updated entities so that they are compatible with EF and Mongo.  Can't use ObjectIds :(

---
+++ UtahPlanners.POC/Repository/Mappings/PropertyContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using UtahPlanners.POC.Domain;
+
+namespace UtahPlanners.POC.Repository.Mappings
+{
+    public class PropertyContext : DbContext
+    {
+        public DbSet<Property> Properties { get; set; }
+
+        public PropertyContext()
+            : base("PropertiesConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var prop = modelBuilder.Entity<Property>();
+            
+            prop.HasKey<Guid>(p => p.Id);
+            prop.Property(p => p.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            prop.Property(p => p.Id).HasColumnType("uniqueidentifier");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
